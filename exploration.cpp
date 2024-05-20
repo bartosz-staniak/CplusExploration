c114c43e1b6dfdf7cbd5a4f637f893a846eab36c
bs <bartosz.staniak@gmail.com>
2024-05-20 19:34:02 +0200

    added the new line sign

--- exploration.cpp
+++ exploration.cpp
@@ -7,7 +7,7 @@ int main ()
 
 char quitApp;
  
-cout << "Press any letter key then Enter key to end the program.";
+cout << "Press any letter key then Enter key to end the program. \n";
 cin >> quitApp;
 
 if (quitApp == 'y')
